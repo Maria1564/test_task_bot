6f94e9cf12ed3e11232ccacd71041149f167005a
Maria <salyginamaria156@gmail.com>
2025-02-26 20:22:57 +0300

    начало

---
+++ axiosConfig.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const apiClient  = axios.create({
+  baseURL: 'https://bothubq.com/api/v2',
+  headers: {
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIxNjBhOTAxLWJiMzYtNDIzZS05NGQ1LWVmMzM5YTcxMDQwNSIsImlzRGV2ZWxvcGVyIjp0cnVlLCJpYXQiOjE3NDAwNjA3NDEsImV4cCI6MjA1NTYzNjc0MX0.JYrAECA8EpzptOqtKIyq7gJWf83hburC9S25yF5Xt3k',
+  },
+});
+
+export default apiClient
